c484ecdf806a03d5ddd6baa6a1dd5c6ba4c90b71
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 14:49:05 +0300

    First Commit

---
+++ condition.js
@@ -0,0 +1,45 @@
+// İF, ELSE İF, ELSE
+// if(condition){
+//     The code that will run when the condition is met.
+// }
+// else if(other condition){
+//     The code that will run when the other condition is met.
+// }
+// else{
+//     The code that will run when none of these conditions are met.
+// }
+// var age=17
+// if(age<18){
+//     console.log("CHILD")
+// }
+// else if(age<65){
+//     console.log("ADULT")
+// }
+// else if(age>=65){
+//     console.log("SO OLD")
+// }
+
+// SWİTCH CASE
+
+// switch(choice){
+//     case first choice:
+//         The code to be executed.
+//         break;
+//     case second:
+//         The code to be executed.
+//         break;
+//         .
+//         .
+//         .
+//         .
+//         .
+//     default:
+//         Code to run when no selection is made
+//         break;
+// }
+
+// Ternary operator:
+// let age=17
+// let situation=yas>=18?
+// "ADULT":"CHILD"
+// console.log(situation)
